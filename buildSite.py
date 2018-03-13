b8135f20411f774b9010f60e771948ec868be117
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-13 10:55:12 -0700

    update with build script

---
+++ buildSite.py
@@ -0,0 +1,19 @@
+from yattag import Doc
+
+doc, tag, text = Doc().tagtext()
+
+with tag('html'):
+    doc.stag('img', src="logo.png", width="1328", height="360")
+    with tag('body'):
+        with tag('h1'):
+            text('The Maslow Community Garden')
+        with tag('p'):
+            text('This page is a place for community driven open source projects to live')
+
+
+
+print(doc.getvalue())
+
+f = open('index.html','w')
+f.write(doc.getvalue())
+f.close()
