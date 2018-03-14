d7e6265adc6cf83806d914065a4948fab06e2531
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-14 11:26:51 -0700

    div is clickable

--- buildSite.py
+++ buildSite.py
@@ -12,6 +12,8 @@ print projects
 doc, tag, text = Doc().tagtext()
 
 with tag('html'):
+    with tag('head'):
+        doc.stag('link',rel='stylesheet', href='styles.css')
     doc.stag('img', src="logo.png", width="664", height="180")
     with tag('body'):
         with tag('h1'):
@@ -31,14 +33,18 @@ with tag('html'):
                 print "readme url: "
                 print readmeUrl
                 linesInReadme = urllib2.urlopen(readmeUrl)
-                for line in linesInReadme: 
-                    if line[0] is '#':
-                        with tag('h1'):
-                            text(line[1:])
-                    elif line[0] is not '!':
-                        with tag('p'):
-                            text(line)
-                doc.stag('img', src= project + '/master/mainpicture.jpg')
+                
+                #this creates a boxed representation of the project
+                with tag('a', href="http://example.com"):
+                    with tag('div', klass = 'boxed', style="width: 400px; float: left;"):
+                        for line in linesInReadme: 
+                            if line[0] is '#':
+                                with tag('h1'):
+                                    text(line[1:])
+                            elif line[0] is not '!':
+                                with tag('p'):
+                                    text(line)
+                        doc.stag('img', src= project + '/master/mainpicture.jpg')
             except Exception as e:
                 print "\n\n\n\n@#$#@$@"
                 print project
