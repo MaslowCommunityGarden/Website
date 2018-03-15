cd7e038b208c833c29a190309dd5d84824ca01d7
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-15 15:51:40 -0700

    shuffle names

--- create_new_project.php
+++ create_new_project.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+Project name: <?php echo $_GET["projectName"]; ?><br>
+Project description: <?php echo $_GET["projectDescription"]; ?>
 
 </body>
 </html>
