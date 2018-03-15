081d8faa4e261d0952fef3d093235eeafc68427c
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-15 16:21:52 -0700

    read managment style too

--- create_new_project.php
+++ create_new_project.php
@@ -2,7 +2,9 @@
 <body>
 
 Project name: <?php echo $_GET["projectName"]; ?><br>
-Project description: <?php echo $_GET["projectDescription"]; ?>
+Project description: <?php echo $_GET["projectDescription"]; ?><br>
+Management style: <?php echo $_GET["managementStyle"]; ?><br>
+GitHub user name: <?php echo $_GET["githubUser"]; ?><br>
 
 </body>
 </html>
