38c7e9e0b36b3800c84dd9f566a765e83ba7c1a6
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-16 12:03:26 -0700

    test

--- create_new_project.php
+++ create_new_project.php
@@ -6,6 +6,7 @@ GitHub user: <?php echo $_POST["githubUser"]; ?><br>
 
 
 <?php
+//Upload the image file
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["projectImage"]["name"]);
 $uploadOk = 1;
@@ -28,6 +29,57 @@ if (file_exists($target_file)) {
 }
 // Check file size
 if ($_FILES["projectImage"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["projectImage"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["projectImage"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+        echo $target_file;
+    }
+}
+?>
+
+<br>
+
+<br>
+
+<?php
+//Upload the zip file
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["projectFiles"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["projectFiles"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["projectFiles"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
