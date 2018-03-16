7a1fa827e01b0f20ef0b2bdf2dad9d637a6302d7
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-16 11:45:23 -0700

    change file name

--- create_new_project.php
+++ create_new_project.php
@@ -5,12 +5,12 @@ Project description: <?php echo $_POST["projectDescription"]; ?><br>
 
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["projectImage"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["projectImage"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -25,7 +25,7 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["projectImage"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
@@ -40,8 +40,8 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["projectImage"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["projectImage"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
         echo $target_file;
