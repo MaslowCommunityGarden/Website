9d47e72a85f9b51e755e0eaed3c9b04adbbfe4bb
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-16 10:39:57 -0700

    test

--- create_new_project.php
+++ create_new_project.php
@@ -1,25 +1,11 @@
-<html>
-<body>
-
-Project name: <?php echo $_GET["projectName"]; ?><br>
-Project description: <?php echo $_GET["projectDescription"]; ?><br>
-Management style: <?php echo $_GET["managementStyle"]; ?><br>
-GitHub user name: <?php echo $_GET["githubUser"]; ?><br>
-
-
-
-
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["projectPicture"]["name"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-echo $target_file;
-
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["projectPicture"]["tmp_name"]);
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -28,49 +14,4 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["projectPicture"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
 ?>
-
-<?php
-// run the script which will create the repository
-$output=shell_exec('sh /var/www/html/createRepo.sh');
-echo $output;
-?>
-
-<br>
-
-<br>
-
-<?php
-echo $$target_dir
-echo $target_file
-?>
-
-</body>
-</html>
