80a50615890259daa83cdab3def77cabb7ccd270
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-16 12:04:38 -0700

    test

--- create_new_project.php
+++ create_new_project.php
@@ -94,8 +94,8 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["projectImage"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["projectImage"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["projectFiles"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["projectFiles"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
         echo $target_file;
