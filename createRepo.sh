353e792f402ae7183fd70ba542b66e00b7d5c5d9
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-20 13:55:47 -0700

    tst

--- createRepo.sh
+++ createRepo.sh
@@ -7,11 +7,11 @@ cd /var/www/html
 
 echo "after cd"
 
-#sudo unzip /var/www/html/uploads/userUpload.zip -d /var/www/html/uploads
+unzip /var/www/html/uploads/userUpload.zip -d /var/www/html/uploads
 
 #sudo rm /var/www/html/uploads/userUpload.zip 
 
-sudo python createRepo.py
+python createRepo.py
 
 echo "after python"
 
