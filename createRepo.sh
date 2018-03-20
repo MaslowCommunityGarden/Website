73fd21a6689768047677cc80a8c932dcbd1edf92
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-20 13:08:53 -0700

    test

--- createRepo.sh
+++ createRepo.sh
@@ -5,12 +5,12 @@ cd /var/www/html
 
 sudo unzip /var/www/html/uploads/userUpload.zip -d /var/www/html/uploads
 
-#sudo rm /var/www/html/uploads/userUpload.zip 
+sudo rm /var/www/html/uploads/userUpload.zip 
 
 sudo python createRepo.py
 
 cd /var/www/html/uploads
 
-#find . -maxdepth 1 -name \* -type f -printf "deleting %P\n" -delete
+find . -maxdepth 1 -name \* -type f -printf "deleting %P\n" -delete
 
 
