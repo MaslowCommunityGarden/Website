d215e93aac28ff36356abf728c25b1dde970a4fd
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-19 17:35:32 -0700

    add github login

--- createRepo.py
+++ createRepo.py
@@ -1,4 +1,15 @@
-print "this python scipt ran"
+from github import Github
+
+file = open("../gitlogin.txt", "r") 
+logins = file.readlines() 
+userName = logins[0].replace('\n', '')
+password = logins[1]
+
+# using username and password
+g = Github(userName, password)
+org = g.get_organization('MaslowCommunityGarden')
+
+repo = org.create_repo("auto login from server test")
 
 f = open('/var/www/html/uploads/testScriptRan.txt','w')
 f.write("This is some text!")
