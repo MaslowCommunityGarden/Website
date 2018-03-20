28a132646a375dc53aa6218b2899dfe5cf249d75
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-20 12:51:43 -0700

    test

--- createRepo.sh
+++ createRepo.sh
@@ -11,6 +11,6 @@ sudo python createRepo.py
 
 cd /var/www/html/uploads
 
-#find . -maxdepth 1 -name \* -type f -printf "deleting %P\n" -delete
+find . -maxdepth 1 -name \* -type f -printf "deleting %P\n" -delete
 
 
--- create_new_project.php
+++ create_new_project.php
@@ -116,6 +116,6 @@ if ($uploadOk == 0) {
 
 <?php
     // run the script which will create the repository
-    $output=shell_exec('sh /var/www/html/createRepo.sh > /home/ubuntu/scriptlog.txt');
+    $output=shell_exec('sh /var/www/html/createRepo.sh >> /home/ubuntu/scriptlog.txt');
     echo $output;
 ?>
