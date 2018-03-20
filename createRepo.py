fe28508006d7758c1811376b665378cbac0877b5
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-19 17:51:26 -0700

    test

--- createRepo.py
+++ createRepo.py
@@ -1,23 +1,35 @@
 from github import Github
 
-print "python script is running!"
+file = open("/var/www/html/uploads/usrinput.txt", "r")
+userInputs = file.readlines() 
+projectName = logins[0].replace('\n', '')
+projectDescription = logins[1].replace('\n', '')
+managementStyle = logins[2].replace('\n', '')
+githubUser = logins[3].replace('\n', '')
+
+print "Project name: "
+print projectName
+print "Project description"
+print projectDescription
+print "Managment style"
+print managementStyle
+print "User name"
+print githubUser
 
 file = open("/home/ubuntu/gitlogin.txt", "r") 
-print file
 logins = file.readlines() 
-print logins
 userName = logins[0].replace('\n', '')
 password = logins[1].replace('\n', '')
 
 # using username and password
 g = Github(userName, password)
 org = g.get_organization('MaslowCommunityGarden')
-print org
 
-org.create_repo("auto login from server test")
+#org.create_repo("auto login from server test")
 
 
 f = open('/var/www/html/uploads/testScriptRan.txt','w')
 f.write("This is some text!")
 print f
 f.close()
+
