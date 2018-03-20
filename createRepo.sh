f76b9031e9ac93231be3a58f12d5327aad6e5ad6
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-20 13:54:42 -0700

    tet

--- createRepo.sh
+++ createRepo.sh
@@ -5,11 +5,15 @@ echo "got to the beginning of the script"
 
 cd /var/www/html
 
+echo "after cd"
+
 #sudo unzip /var/www/html/uploads/userUpload.zip -d /var/www/html/uploads
 
 #sudo rm /var/www/html/uploads/userUpload.zip 
 
 sudo python createRepo.py
+
+echo "after python"
 
 cd /var/www/html/uploads
 
