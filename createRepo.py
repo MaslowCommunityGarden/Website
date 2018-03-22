67dd2ba15def7e2b8f419e25803097f8af37c8a8
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-22 11:34:05 -0700

    test

--- createRepo.py
+++ createRepo.py
@@ -47,7 +47,7 @@ if projectName != "none":
     files = os.listdir('/var/www/html/uploads')
     
     #Clone the newly created repo
-    repoClone = pygit2.clone_repository(repo.clone_url, '/var/www/html/uploads/tmp')
+    repoClone = pygit2.clone_repository(repo.git_url, '/var/www/html/uploads/tmp')
     print "GIT URL: "
     print repo.clone_url
     print "<---"
@@ -66,6 +66,7 @@ if projectName != "none":
     oid = repoClone.create_commit('refs/heads/master', author, commiter, "init commit",tree,[repoClone.head.get_object().hex])
     remote = repoClone.remotes["origin"]
     remote.credentials = credentials
+    remote.set_push_url("origin", repo.clone_url)
     signature = author
     remote.push(['refs/heads/master'])
     
