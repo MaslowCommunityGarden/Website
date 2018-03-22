d718590b717e6e13108b6606420ad51c4382f8ab
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-22 10:39:44 -0700

    test

--- createRepo.sh
+++ createRepo.sh
@@ -13,8 +13,8 @@ python createRepo.py
 
 cd /var/www/html/uploads
 
-#rm -r -f /var/www/html/uploads/tmp
+rm -r -f /var/www/html/uploads/tmp
 
-#find . -maxdepth 1 -name \* -type f -printf "deleting %P\n" -delete
+find . -maxdepth 1 -name \* -type f -printf "deleting %P\n" -delete
 
 
