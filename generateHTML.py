6012c0edb2140c5f1ecf14694c80859fc4c89ec2
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-23 10:16:14 -0700

    test

--- generateHTML.py
+++ generateHTML.py
@@ -232,7 +232,18 @@ class GenerateHTML:
 
                             "<div id=\"Forums\" class=\"tabcontent\">"
                               "<h3>Forums</h3>"
-                              "<p>This is where the forums for the community will be (coming soon)</p>"
+                              "<div id='discourse-comments'></div>"
+
+                              "<script type=\"text/javascript\">"
+                                "DiscourseEmbed = { discourseUrl: 'https://forums.maslowcnc.com/',"
+                                                   "discourseEmbedUrl: 'http://maslowcommunitygarden.org/TestProject.html' };"
+
+                                "(function() {"
+                                  "var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;"
+                                  "d.src = DiscourseEmbed.discourseUrl + 'javascripts/embed.js';"
+                                  "(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);"
+                                "})();"
+                              "</script>"
                             "</div>"
 
                             "<div id=\"Buy\" class=\"tabcontent\">"
