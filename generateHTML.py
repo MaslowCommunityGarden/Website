2e49d44d2fb3b7e9e038475f3a7e67f54c2b8fa4
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-23 10:39:43 -0700

    test

--- generateHTML.py
+++ generateHTML.py
@@ -236,7 +236,7 @@ class GenerateHTML:
 
                               "<script type=\"text/javascript\">"
                                 "DiscourseEmbed = { discourseUrl: 'https://forums.maslowcnc.com/',"
-                                                   "discourseEmbedUrl: 'http://maslowcommunitygarden.org/'" + project.projectFile + " };"
+                                                   "discourseEmbedUrl: 'http://maslowcommunitygarden.org/" + project.projectFile + "' };"
 
                                 "(function() {"
                                   "var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;"
