02ac35b9b01c40fe6700d68ae9a7d927e534f415
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-29 11:00:49 -0700

    test

--- Scripts/gardenRobot.py
+++ Scripts/gardenRobot.py
@@ -7,3 +7,9 @@ password = logins[1].replace('\n', '')
 
 # using username and password
 g = Github(userName, password)
+
+#Open list of tracked projects
+file = open("/var/www/html/trackedProjects.txt", "r") 
+trackedProjects = file.readlines() 
+
+print trackedProjects
