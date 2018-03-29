0d4b1b87de86640fade653efa1dc07f1b8c5e505
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-29 13:33:20 -0700

    test

--- Scripts/gardenRobot.py
+++ Scripts/gardenRobot.py
@@ -18,45 +18,51 @@ repos = org.get_repos()
 
 for repo in repos:
     
+    print "robot processing repo: " + repo.name
+    
     #read the ROBOT.md file to see if the robot should interact with this project
     
-    #find the URL to the file
-    trackedURL  = repo.html_url
-    rawPath     = trackedURL.split('.com')[-1]
-    rawPath     = "https://raw.githubusercontent.com" + rawPath
-    rawPath     = rawPath.replace('\n', '')
-    robotURL    = rawPath + '/master/ROBOT.md'
-    robotURL    = "".join(robotURL.split())
-    
-    #read the file
-    text        = urllib2.urlopen(robotURL)
-    robotText   = text.read()
-    
-    print "Robot text:"
-    print robotText
-    
-    if 'communityManaged' in robotText:
-        print "This project is community managed"
-    else:
-        print "this project is not community managed"
-    
-    openPullRequests = repo.get_pulls()
-    for pullRequest in openPullRequests:
-        print (repo.name)
-        print pullRequest.title
+    try:
+        #find the URL to the file
+        trackedURL  = repo.html_url
+        rawPath     = trackedURL.split('.com')[-1]
+        rawPath     = "https://raw.githubusercontent.com" + rawPath
+        rawPath     = rawPath.replace('\n', '')
+        robotURL    = rawPath + '/master/ROBOT.md'
+        robotURL    = "".join(robotURL.split())
         
-        pullRequestAlreadyRespondedTo = False
+        #read the file
+        text        = urllib2.urlopen(robotURL)
+        robotText   = text.read()
         
-        print "mergable"
-        print pullRequest.mergeable
-        print "user:"
-        print pullRequest.user
-        print "comments:"
-        print pullRequest.comments
-        print "review comments:"
-        print pullRequest.review_comments
-        comments = pullRequest.get_comments()
-        print comments
-        for aComment in comments:
-            print "Comment Text:"
-            print aComment.body
+        print "Robot text:"
+        print robotText
+        
+        if 'communityManaged' in robotText:
+            print "This project is community managed"
+        else:
+            print "this project is not community managed"
+        
+        openPullRequests = repo.get_pulls()
+        for pullRequest in openPullRequests:
+            print (repo.name)
+            print pullRequest.title
+            
+            pullRequestAlreadyRespondedTo = False
+            
+            print "mergable"
+            print pullRequest.mergeable
+            print "user:"
+            print pullRequest.user
+            print "comments:"
+            print pullRequest.comments
+            print "review comments:"
+            print pullRequest.review_comments
+            comments = pullRequest.get_comments()
+            print comments
+            for aComment in comments:
+                print "Comment Text:"
+                print aComment.body
+    except exception as e:
+        print "This repo does not have a ROBOT.md file"
+        print e
