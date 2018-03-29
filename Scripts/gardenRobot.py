b5750dfb79b21fe309ef955995a3ad1ba7351af1
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-29 13:27:22 -0700

    read robot file

--- Scripts/gardenRobot.py
+++ Scripts/gardenRobot.py
@@ -16,6 +16,24 @@ org = g.get_organization('MaslowCommunityGarden')
 repos = org.get_repos()
 
 for repo in repos:
+    
+    #read the ROBOT.md file to see if the robot should interact with this project
+    
+    #find the URL to the file
+    trackedURL  = repo.html_url
+    rawPath     = trackedURL.split('.com')[-1]
+    rawPath     = "https://raw.githubusercontent.com" + rawPath
+    rawPath     = rawPath.replace('\n', '')
+    robotURL    = rawPath + '/master/README.md'
+    robotURL    = "".join(robotURL.split())
+    
+    #read the file
+    text        = urllib2.urlopen(robotURL)
+    robotText   = text.read()
+    
+    print "Robot text:"
+    print robotText
+    
     openPullRequests = repo.get_pulls()
     for pullRequest in openPullRequests:
         print (repo.name)
@@ -31,8 +49,6 @@ for repo in repos:
         print pullRequest.comments
         print "review comments:"
         print pullRequest.review_comments
-        print "comment zero:"
-        print pullRequest.get_comment(0)
         comments = pullRequest.get_comments()
         print comments
         for aComment in comments:
