6e57e8b7ad08094f78e74b7330d2471ba78abe9f
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-04-24 16:12:32 -0700

    Run robot against maslow cnc repos

--- Scripts/gardenRobot.py
+++ Scripts/gardenRobot.py
@@ -9,23 +9,20 @@ logins = file.readlines()
 userName = logins[0].replace('\n', '')
 password = logins[1].replace('\n', '')
 
-# using username and password
+# Login to github
 g = Github(userName, password)
 org = g.get_organization('MaslowCommunityGarden')
-
 repos = org.get_repos()
-
-file = open("/home/ubuntu/gitlogin.txt", "r") 
-logins = file.readlines() 
-userName = logins[0].replace('\n', '')
-password = logins[1].replace('\n', '')
 
 robot = Robot()
 
 for repo in repos:
-    
-    #print "\n\nRobot processing repo: " + repo.name
-    
-    #read the ROBOT.md file to see if the robot should interact with this project
-    
+    #handle pull requests by voting through the commuinity garden robot
+    robot.voteOnPRs(repo)
+
+org = g.get_organization('MaslowCNC')
+repos = org.get_repos()
+
+for repo in repos:
+    #handle pull requests by voting through the commuinity garden robot
     robot.voteOnPRs(repo)
