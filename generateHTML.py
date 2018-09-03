d3901de241c58481d5c4a60a68f77bf515c23a7c
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-09-03 12:32:35 -0700

    Clean up footer
    The footer on the community garden was pretty ugly and had some links which didn't resize nicely

--- generateHTML.py
+++ generateHTML.py
@@ -299,17 +299,7 @@ class GenerateHTML:
         "<footer class='footer-basic-centered' style='clear:both;'>"
             "<div class='inner-footer'>"
 
-                 "<p class='footer-company-motto three-col'>The garden is a project of the <a href='http://www.maslowcnc.com'>Maslow CNC</a> community.</p>"
-    
-                "<div class='footer-links'>"
-                    "<a href='http://maslowcommunitygarden.org/howdoesthegardenwork.html' class='button'>How</a>"
-                    "  "
-                    "<a href='http://maslowcommunitygarden.org/addaproject.html' class='button'>Add</a>"
-                    "  "
-                    "<a href='http://www.maslowcnc.com/' class='button'>Maslow CNC</a>"
-                    "  "
-                    "<a href='http://www.maslowcnc.com/forums' class='button'>Forums</a>"
-                "</div>"
+                "<p class='footer-company-motto three-col'>The garden is a project of the <a href='http://www.maslowcnc.com'>Maslow CNC</a> community.</p>"
     
                 "<p class='footer-company-name three-col'>All content available under license of creator.</p>"
             "</div>"
@@ -476,16 +466,6 @@ class GenerateHTML:
                         "<div class='inner-footer'>"
 
                             "<p class='footer-company-motto three-col'>The garden is a project of the <a href='http://www.maslowcnc.com'>Maslow CNC</a> community.</p>"
-
-                            "<div class='footer-links'>"
-                                "<a href='http://maslowcommunitygarden.org/howdoesthegardenwork.html' class='button'>How</a>"
-                                "  "
-                                "<a href='http://maslowcommunitygarden.org/addaproject.html' class='button'>Add</a>"
-                                "  "
-                                "<a href='http://www.maslowcnc.com/' class='button'>Maslow CNC</a>"
-                                "  "
-                                "<a href='http://www.maslowcnc.com/forums' class='button'>Forums</a>"
-                            "</div>"
 
                             "<p class='footer-company-name three-col'>All content available under license of creator.</p>"
 
